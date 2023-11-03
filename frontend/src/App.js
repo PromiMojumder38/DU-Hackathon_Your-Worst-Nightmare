35f76dc39c5b3e76a03b04e298f9ae4f03c3dc26
PromiMojumder38 <promi38@student.sust.edu>
2023-11-04 03:53:31 +0600

    add: map for selected cities

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,9 @@ import AQI from "./Pages/AQIDetails/index";
 import Top from "./Pages/Top10/App";
 import Ph from "./Pages/PhValues/index";
 import Map from "./Pages/map/index";
+import Gdp from "./Pages/GDP/CountryCodeLookUp";
+import Gdp2 from "./Pages/GDP/DataTables";
+import Gdp3 from "./Pages/GDP/useFetchCountryData";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -16,6 +19,9 @@ function App() {
         <Route path="/top" element={<Top />}></Route>
         <Route path="/ph" element={<Ph />}></Route>
         <Route path="/map" element={<Map />}></Route>
+        <Route path="/gdp" element={<Gdp />}></Route>
+        <Route path="/gdp2" element={<Gdp2 />}></Route>
+        <Route path="/gdp3" element={<Gdp3 />}></Route>
       </Routes>
     </BrowserRouter>
   );
