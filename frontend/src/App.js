ed6b0edacefa6d4e0503670257f156331ffe2e45
Sumonta Saha Mridul <61287791+Sumonta056@users.noreply.github.com>
2023-11-04 02:21:41 +0600

    [add] Intregated Map using User Location

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,8 @@
 import React from "react";
-
 import Home from "./components/Home/Home";
-//import "bootstrap/dist/css/bootstrap.min.css";
-// import Student from "./components/CRUD/practice";
-// import Create from "./components/CRUD/CreateStudent";
-// import Update from "./components/CRUD/UpdateStudent";
-
 import AQI from "./Pages/AQIDetails/index";
 import Top from "./Pages/Top10/App";
 import Ph from "./Pages/PhValues/index";
-
 import Map from "./Pages/map/index";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -19,7 +12,6 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-
         <Route path="/aqi" element={<AQI />}></Route>
         <Route path="/top" element={<Top />}></Route>
         <Route path="/ph" element={<Ph />}></Route>
